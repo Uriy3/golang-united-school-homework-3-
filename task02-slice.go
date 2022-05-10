eadae1e228049a0472415fe52c3e35c541b52cb6
Uriy3 <99492882+Uriy3@users.noreply.github.com>
2022-05-10 21:56:26 +0300

    Update task02-slice.go

--- task02-slice.go
+++ task02-slice.go
@@ -2,9 +2,15 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
-	num := len(input)
-        for i := num - 1; i >= 0; i-- {
-            result = append(result, input[i])
+	keys := make([]int, len(input))
+        i := 0
+        for k := range input {
+            keys[i] = k
+            i++
+        }
+        sort.Ints(keys)
+        for , k := range keys {
+            result = append(result, input[k])
         }
         return result
     }
